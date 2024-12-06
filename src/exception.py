a3968e478842c184adc2938c02a870fb615a8b55
adityavarma99 <adityavarma759@gmail.com>
2024-12-06 22:23:45 +0530

    EDA and model trainer

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()                 ## this line tells error details is which line and which file the exception has occured
