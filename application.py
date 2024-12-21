6e62f270d437c6f58a7df2b1e4c32cfc62d771f2
Aditya Varma <102342213+adityavarma99@users.noreply.github.com>
2024-12-21 19:29:18 +0530

    Update application.py
    changed port from 5000 to 2000

--- application.py
+++ application.py
@@ -41,6 +41,6 @@ def predict_datapoint():                                       # calling the fuc
         return render_template('home.html',results=results[0])            # results is printed from home.html
     
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5000)
+    app.run(host="0.0.0.0",port=2000)
 
 
